0ba36a1239f8fb97a30f86df886caae931487214
peterphipps <peterjohnphipps@gmail.com>
2016-11-17 21:05:58 +0000

    Updated to work with new datagrabber

--- EmotionCommotion/backend/featureExtractors/meanZeroCrossing.py
+++ EmotionCommotion/backend/featureExtractors/meanZeroCrossing.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import librosa
 from datagrabber import extractAndSave
 
 IEMOCAP_LOCATION = "../../../../local"
 
-def meanZeroCrossing(filename):
-    audio,sr = librosa.load(filename,sr=None)
-    return librosa.feature.zero_crossing_rate(audio).mean()
+def zerocrossing(frame, audiofile):
+    i = 0
+    pos = frame[0]
+    for x in range(0, len(frame)-1):
+        if (frame[i]*frame[i+1] > 0):
+            i += 1
+    return [i]
 
-extractAndSave(meanZeroCrossing,IEMOCAP_LOCATION,2,5,False)
+extractAndSave(zerocrossing, ['zerocrossing'],IEMOCAP_LOCATION,2)
