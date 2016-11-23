3b19678300f519543c417bf0023defa5e3195fda
Peter Phipps <peterjohnphipps@gmail.com>
2016-11-23 13:13:50 +0000

    fixed f0

--- EmotionCommotion/backend/featureExtractors/f0.py
+++ EmotionCommotion/backend/featureExtractors/f0.py
@@ -5,18 +5,24 @@ Created on Sat Nov 12 12:04:22 2016
 
 @author: Tom
 """
-from importlib.machinery import SourceFileLoader
+#from importlib.machinery import SourceFileLoader
+
+import sys
+sys.path.append('/dcs/project/emotcomm/EmotionCommotion/EmotionCommotion/backend/sourceFiles/')
+import thinkplot as tp
+import thinkdsp as td
 from datagrabber import extractAndSave
 
 IEMOCAP_LOCATION = "../../../../local"
 
-
+threshold=0.5
 #assuming for now that thinkdsp and clip is in the same directory
-def f0(filename, threshold=0.5):
-   # thinkplot = SourceFileLoader("thinkplot", dirPath + "thinkplot.py").load_module()
-    thinkdsp = SourceFileLoader("thinkdsp", "../sourceFiles/thinkdsp.py").load_module()
+def f0(frame, audiofile):
+    #thinkplot = SourceFileLoader("thinkplot", dirPath + "thinkplot.py").load_module()
+    #thinkdsp = SourceFileLoader("thinkdsp", "../sourceFiles/thinkdsp.py").load_module()
 
-    clip = thinkdsp.read_wave(filename)
+    #clip = td.read_wave(filename)
+    clip = td.Wave(frame)
     spectrum = clip.make_spectrum()
 
     #sorted_by_Hz = sorted(spectrum.peaks(), key=lambda tup: tup[1])
@@ -39,7 +45,7 @@ def f0(filename, threshold=0.5):
     # selected_pairs is now a list of the pairs which have a prominent amplitude
     sorted_by_Hz = sorted(selected_pairs, key=lambda tup: tup[1])
     # the lowest Hz should now be F0, will need to tweek 0.5
-    return sorted_by_Hz[0][1]
+    return [sorted_by_Hz[0][1]]
 
 #f0('C:/Users/Tom/Documents/GitHub/local/IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro02/Ses01F_impro02_F001.wav',0.5)
 
