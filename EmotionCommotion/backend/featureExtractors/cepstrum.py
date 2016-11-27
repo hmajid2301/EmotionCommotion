4780937cfc0935336cceff8dc35eab448e463ab7
Olly <ollystyles@gmail.com>
2016-11-27 18:14:00 +0000

    removed imaginary part from cepstrum

--- EmotionCommotion/backend/featureExtractors/cepstrum.py
+++ EmotionCommotion/backend/featureExtractors/cepstrum.py
@@ -12,6 +12,7 @@ def cepstrum(frame, filename):
     audio = audio ** 2
     audio = np.log2(audio)
     audio = np.fft.ifft(audio)
+    audio = audio.real
     return [np.amax(audio), np.average(audio), np.var(audio)]
 
 extractAndSave(cepstrum,["max", "mean", "var"],IEMOCAP_LOCATION,2)
