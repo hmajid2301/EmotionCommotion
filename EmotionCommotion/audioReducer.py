8f4780250f7a4bc0c2be7b3633f3acbb29150cb8
tombanham <tombanham@users.noreply.github.com>
2017-02-28 15:04:27 +0000

    Created AudioReducer
    Splits audio into X to X*2 second chunks

---
+++ EmotionCommotion/audioReducer.py
@@ -0,0 +1,69 @@
+import scipy.io.wavfile as wav   # Reads wav file
+import sys
+import csv
+import ntpath
+
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+import sys
+from types import *
+import json
+
+#AUDIOPATH ON EMOTCOMM: "/dcs/project/emotcomm/local/wild_dataset/chopped_and_labelled/"
+MIN_CLIP_LENGTH = 5 #min length of output clips in seconds
+
+def save_clip(start, finish, emmotion, counter, audio, sample_rate):
+	samples = audio[start:finish]
+	output_name = emmotion + "_" + str(counter) + ".wav"
+	wav.write(output_name, sample_rate, samples)
+	print("Saved interval from sample " + str(start) + " to sample " + str(finish) + " at " + output_name + "\n")
+	
+	#add to CSV file
+	fields = [output_name,emmotion]
+	csv_name = 'emmotion_labels.csv'
+	fd = open(csv_name,'a')
+	fd.write(output_name+","+emmotion+"\n")
+	print("Appened to output csv file: " + csv_name)
+
+def audioReducer(audiopath):
+	print("\n")
+	print("Processing audio at: " + audiopath)
+	print("\n")
+	
+	counter = 0
+
+	for filename in os.listdir(audiopath):
+		fullpath = os.path.join(audiopath, filename)
+		if filename.endswith(".wav"):
+			print("Considering: " + fullpath + "\n")
+			
+			emmotion = filename.split('_', 1)[0]
+			[sample_rate, audio] = wav.read(fullpath)
+			
+			audio_length = len(audio)/sample_rate
+			current_length = audio_length
+			sample_position = 0
+			
+			while(current_length >= MIN_CLIP_LENGTH*2):
+				#take MIN_CLIP_LENGTH seconds and write to seperate file
+				start = sample_position * sample_rate
+				finish = (sample_position + MIN_CLIP_LENGTH) * sample_rate
+				save_clip(start, finish, emmotion, counter, audio, sample_rate)
+				counter = counter + 1
+				
+				#move up the sample position so that the same samples aren't written twice
+				sample_position = sample_position + MIN_CLIP_LENGTH
+				current_length = current_length - MIN_CLIP_LENGTH
+				
+			#write remaining seconds to seperate file
+			start = sample_position * sample_rate
+			finish = audio_length * sample_rate
+			save_clip(start, finish, emmotion, counter, audio, sample_rate)
+			
+		else:
+			print("Skipping non-wavfile: " + fullpath + "\n")
+
+audiopath = sys.argv[1]
+audioReducer(audiopath)
